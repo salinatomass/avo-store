3c9b60d5ee4e3afc9bb6164768ff66537bc72dae
Tomás Salina <salinatomass53@gmail.com>
2022-12-26 00:38:37 -0300

    use eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react";
-import Navbar from "../components/Navbar/Navbar";
+import { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar/Navbar'
 
 export default function HomePage() {
-  const [productList, setProductList] = useState<TProduct[]>([]);
+  const [productList, setProductList] = useState<TProduct[]>([])
 
   useEffect(() => {
-    fetch("/api/avo")
-      .then((res) => res.json())
+    fetch('/api/avo')
+      .then(res => res.json())
       .then(({ data }) => setProductList(data))
-      .catch((err) => console.error(err));
-  }, []);
+      .catch(err => console.error(err))
+  }, [])
 
   return (
     <div>
       <Navbar />
       <h1>Platzi Avo</h1>
-      {productList.map((product) => (
+      {productList.map(product => (
         <p key={product.id}>{product.name}</p>
       ))}
     </div>
-  );
+  )
 }
