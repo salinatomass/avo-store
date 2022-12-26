8fdbb5fb59b3f896c68797f9725a1f060d678a07
root <root@NDS-PC.localdomain>
2022-12-25 23:08:04 -0300

    api with nextjs

---
+++ pages/api/avo/index.ts
@@ -0,0 +1,12 @@
+import { IncomingMessage, ServerResponse } from "http";
+import DB from '@database'
+
+const allAvos = async (req: IncomingMessage, res: ServerResponse) => {
+  const db = new DB()
+  const allEntries = await db.getAll()
+  res.statusCode = 200
+  res.setHeader('Content-type', 'application/json')
+  res.end(JSON.stringify({ length: allEntries.length, data: allEntries }))
+}
+
+export default allAvos
