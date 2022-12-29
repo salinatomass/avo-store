48be12f146fb22936d1b5c9edb054abd06d5bc43
Tomás Salina <salinatomass53@gmail.com>
2022-12-29 11:22:55 -0300

    <feat>HomePage: add server side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,26 @@ import Header from '@components/Header/Header'
 import { ProductList } from '@components/Product'
 import { useAppContext } from 'context/AppContext'
 
-export default function HomePage() {
-  const { products } = useAppContext()
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3000/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
 
+  return {
+    props: {
+      productList,
+    },
+  }
+}
+
+interface Props {
+  productList: TProduct[]
+}
+
+export default function HomePage({ productList }: Props) {
   return (
     <div className="wrapper">
       <Header />
-      <ProductList products={products} />
+      <ProductList products={productList} />
     </div>
   )
 }
